1752193ce334dbdb9fb804fefbed4d6e2f5c5ae1
Sam Ritchie <sritchie@twitter.com>
2013-09-16 13:39:58 -0700

    add scheme test. closes #37

---
+++ tormenta-core/src/test/scala/com/twitter/tormenta/scheme/SchemeLaws.scala
@@ -0,0 +1,41 @@
+/*
+ Copyright 2012 Twitter, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package com.twitter.tormenta.scheme
+
+import backtype.storm.topology.TopologyBuilder
+import backtype.storm.testing.{ MockedSources, TestGlobalCount, CompletableSpout }
+import backtype.storm.LocalCluster
+import com.twitter.tormenta.spout.Spout
+import backtype.storm.testing.CompleteTopologyParam
+import backtype.storm.tuple.Values
+import backtype.storm.Testing
+import org.specs._
+import scala.collection.JavaConverters._
+
+object SchemeWithHandlerSpecification extends Specification {
+  "Scheme should handle failure" should {
+    "test failure" in {
+      val f: Array[Byte] => List[String] = b => throw new IllegalArgumentException("decode failed")
+
+      val schemeWithErrorHandler = Scheme(f).withHandler(t => List(t.getMessage))
+      val result = schemeWithErrorHandler.deserialize("test string".getBytes("UTF-8"))
+
+      result.asScala.isEmpty mustBe false   //test fails, returns an empty list
+      result.asScala.head.get(0) must_== "decode failed"
+    }
+  }
+}
