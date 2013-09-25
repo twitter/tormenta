e7c1b7527dc44e55687dca7b9133a9fb40e23b65
Mansur Ashraf <mansoor.ashraf@gmail.com>
2013-09-24 23:17:28 -0500

    fixed typo

--- tormenta-core/src/test/scala/com/twitter/tormenta/TopologyTest.scala
+++ tormenta-core/src/test/scala/com/twitter/tormenta/TopologyTest.scala
@@ -26,7 +26,7 @@ import backtype.storm.Testing
 import org.specs._
 import scala.collection.JavaConverters._
 
-class TopologyTest extends Specification {
+object TopologyTest extends Specification {
   val spout: Spout[Int] = Spout.fromTraversable(List(1,2,3,4,5))
 
   val builder = new TopologyBuilder
